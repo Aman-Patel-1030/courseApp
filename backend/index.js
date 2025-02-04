1eac7246ea0f233d641b44ce712250100446af54
Aman Patel <124379903+Aman-Patel-1030@users.noreply.github.com>
2025-02-05 00:09:27 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(fileUpload({
 
 app.use(
     cors({
-      origin:"http://localhost:5173",
+      origin:"https://course-app-gamma-sable.vercel.app/",
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE"],
       allowedHeaders: ["Content-Type", "Authorization"],
