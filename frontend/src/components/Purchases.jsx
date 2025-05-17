205708d946d1d01d3b06080c56074204d7692c8b
Aman Patel <124379903+Aman-Patel-1030@users.noreply.github.com>
2025-05-17 14:59:18 +0530

    Purchases.jsx

--- frontend/src/components/Purchases.jsx
+++ frontend/src/components/Purchases.jsx
@@ -40,7 +40,7 @@ function Purchases() {
   useEffect(() => {
     const fetchPurchases = async () => {
       try {
-        const response = await axios.get(`${BACKEND_URL}/user/purchases`, {
+        const response = await axios.get(`${BACKEND_URL}/user/purchased`, {
           headers: {
             Authorization: `Bearer ${token}`,
           },
